75542ff1434cca986e1ab5f5e991e68d6e2332e8
kishu2917 <130164560+kishu2917@users.noreply.github.com>
2025-06-25 16:07:41 +0530

    Add files via upload

--- crud.sql
+++ crud.sql
@@ -7,7 +7,11 @@ use d2dc;
  -- (2,'amit',35),
  -- (3,'pradip',55),
  -- (4,'mark',34);
- insert into Student(id,st_name) values
- (11,'sumit'),
- (22,'bhavin');
+-- insert into Student(id,st_name) values
+ -- (11,'sumit'),
+-- (22,'bhavin');
  
+ -- update Student set st_name='pratik' where id = 1;
+ -- update Student set id = 100 where st_name = 'pratik'; 
+-- delete from Student where id =100;
+  delete from Student where age =35;
